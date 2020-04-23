479c4a49fd757f725329b4e8bf6ea628361079d1
Deena1999 <53299197+Deena1999@users.noreply.github.com>
2020-04-22 23:59:44 -0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,1 +1,10 @@
+const mysql=require('mysql2/promise');
 
+const data=mysql.createPool({
+    host:'localhost',
+    user:'root',
+    password:'', //Password for root user
+    database:''  // Schema name as created in mysql.
+  });
+
+module.exports=data;
