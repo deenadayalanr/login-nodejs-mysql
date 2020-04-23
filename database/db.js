3b30ef1ecd4f70b68cc5e5a1ca32a58ff18c512c
Deena1999 <53299197+Deena1999@users.noreply.github.com>
2020-04-23 00:13:33 -0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 const mysql=require('mysql2/promise');
 
 const data=mysql.createPool({
-    host:'localhost',
-    user:'root',
+    host:'', //Your Host name
+    user:'root', //If user is not root mention your user name.  
     password:'', //Password for root user
     database:''  // Schema name as created in mysql.
   });
